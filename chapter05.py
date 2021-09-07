a9e8be0ed56ff7e3df3a9e37e81483204be04cc8
Steve Braich <33675736+steve3p0@users.noreply.github.com>
2021-09-07 04:04:59 -0700

    added jupyter notebook chap5

--- chapter05.py
+++ chapter05.py
@@ -2,6 +2,9 @@
 import math
 import numpy as np
 
+# Refer to the example XOR neural network in Figure 5.4 on page 71.
+# Refer to Table 5.2, page 78
+
 W = np.array([[3, 4], [2, 3]])
 b = np.array([-2, -4])
 W2 = np.array([5, -5])
@@ -15,6 +18,12 @@ def sigmoid(x):
 def sigmoid_derivative(x):
     return sigmoid(x) * (1 - sigmoid(x))
 
+# These are the input x0, x1:
+# x0, x1,   t
+#  0,  0    0
+#  0,  1    1
+#  1,  0    1
+#  1,  1    0
 x = np.array([1, 0])
 t = np.array([1])
 
@@ -26,9 +35,10 @@ h = sigmoid(s)
 z = W2.dot(h) + b2
 y = sigmoid(z)
 
+############################################################
 # 5.7.3 Backward Computation
 
-error = .5 * (t - y)**2
+error = 1/2 * (t - y)**2
 mu = 1
 
 delta_2 = (t - y) * sigmoid_derivative(z)
@@ -39,6 +49,22 @@ delta_1 = W * delta_2 * sigmoid_derivative(s)
 delta_W = mu * np.array([ delta_1 ]).T * x
 delta_b = mu * delta_1
 
+d_error_d_y = t - y
 
+d_y_d_z = sigmoid_derivative( z )
+d_error_d_z = d_error_d_y * d_y_d_z
 
+d_z_d_W2 = h
+d_error_d_W2 = d_error_d_z * d_z_d_W2
 
+d_z_d_b2 = 1
+d_error_d_b2 = d_error_d_z * d_z_d_b2
+d_z_d_h = W2
+d_error_d_h = d_error_d_z * d_z_d_h
+
+d_s_d_h = sigmoid_derivative( s )
+d_error_d_s = d_error_d_h * d_s_d_h
+d_W_d_s = x
+d_error_d_W = np.array([ d_error_d_s ]).T * d_W_d_s
+d_b_d_s = 1
+d_error_d_b = d_error_d_s * d_b_d_s
